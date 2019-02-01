de317e77d6f0c6b0ce2bfce1922c36ffa54682f7
Leroy Ware <leroy@deeprapid.com>
2019-02-01 14:30:18 -0600

    Added Topics and User endpoints
    * Added update, delete, and list topic endpoints
    * Added user registration endpoint
    * Minor re-factor of existing routes to check for missing parameters

--- api/subTopicList/handler.js
+++ api/subTopicList/handler.js
@@ -4,24 +4,33 @@ const fxlisten = require("@fxlisten/core");
 const { Category } = fxlisten.dynamo;
 
 const list = async (event, context) => {
-  console.log(event.queryStringParameters.topic);
-  var params = {
-    FilterExpression: "parent = :parent",
-    ExpressionAttributeValues: {
-      ":parent": event.queryStringParameters.topic
-    }
-  };
-  const categories = await Category.scan(params).exec();
-  const topics = categories.map(category => {
-    return {
-      topic: category.name,
-      image_url: ""
+  const { topic } = event.queryStringParameters;
+  if (topic) {
+    var params = {
+      FilterExpression: "parent = :parent",
+      ExpressionAttributeValues: {
+        ":parent": topic
+      }
     };
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify(topics)
-  };
+    const categories = await Category.scan(params).exec();
+    const topics = categories.map(category => {
+      return {
+        topic: category.name,
+        image_url: ""
+      };
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(topics)
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Missing URL parameters"
+      })
+    };
+  }
 };
 
 module.exports = {
--- api/topics/handler.js
+++ api/topics/handler.js
@@ -1,39 +1,86 @@
 "use strict";
 
-// This is just a stub. The handler has not been implemented.
+const fxlisten = require("@fxlisten/core");
+const { Category, User } = fxlisten.dynamo;
+
+const list = async (event, context) => {
+  const { userId } = event.queryStringParameters;
+  if (userId) {
+    const user = await User.get(userId);
+    const params = user.topics.split(",").map(id => {
+      return { id: id };
+    });
+    const categories = await Category.batchGet(params);
+    const topics = categories.map(category => {
+      return {
+        topic: category.name,
+        image_url: ""
+      };
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(topics)
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Missing URL parameters"
+      })
+    };
+  }
+};
 
 const update = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
+  const { userId, topic } = event.queryStringParameters;
+  if (userId && topic) {
+    let user = await User.get(userId);
+    let topics = user.topics.split(",");
+    if (!topics.includes(topic)) topics.push(topic);
+    user.topics = topics.join(",");
+    await user.save();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Topic successfully added to list."
+      })
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Missing URL parameters"
+      })
+    };
+  }
 };
 
 const del = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
-};
-
-const list = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
+  const { userId, topic } = event.queryStringParameters;
+  if (userId && topic) {
+    let user = await User.get(userId);
+    let topics = user.topics.split(",");
+    topics = topics.filter(id => id !== topic);
+    user.topics = topics.join(",");
+    await user.save();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Topic successfully removed from list."
+      })
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Missing URL parameters"
+      })
+    };
+  }
 };
 
 module.exports = {
+  list,
   update,
-  del,
-  list
+  del
 };
