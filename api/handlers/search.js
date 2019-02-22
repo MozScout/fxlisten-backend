213fb083eb78f8ee8b86c294e0ad4cfdb68e06b7
Leroy Ware <leroyjware@users.noreply.github.com>
2019-02-22 12:53:39 -0600

    Added search integration with Mozcast (#22)
    * Added search integration with Mozcast
    
    * Added /search route that queries Mozcast for episodes and podcasts
    
    * Added authorizer to search
    
    * Update to response structure
    
    * Adds audioUrl property to result for episodes
    * Adds feedUrl property to result for podcasts

--- api/handlers/search.js
+++ api/handlers/search.js
@@ -1,14 +1,45 @@
 'use strict';
 
-module.exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
-    })
-  };
+const { response, mozcast } = require('@fxlisten/core');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.handler = async (event, context) => {
+  const { q } = event.queryStringParameters;
+  const SEARCH = ` 
+    query ($query: String!) {
+	  search(query: $query) {
+	  	__typename
+	    ... on Podcast {
+	      title
+	      description
+	      feedUrl
+	      image
+	    }
+	    __typename
+	    ... on Episode {
+	      title
+	      description
+	      audioUrl
+	      image
+	    }
+	  }
+    }
+  `;
+  const { search } = await mozcast.graphql(SEARCH, { query: q });
+  const results = search.map(result => {
+  	const item = {
+      id: result.id,
+      title: result.title,
+      description: result.description,
+      imageUrl: result.image,
+      type: result.__typename.toLowerCase()
+    };	
+    if (result.__typename == 'Podcast') {
+    	item.feedUrl = result.feedUrl;
+    }
+    if (result.__typename == 'Episode') {
+    	item.audioUrl = result.audioUrl;
+    }
+    return item;
+  });
+  return response.success(results);
 };
